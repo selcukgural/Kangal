0c2c92b5587aaa26c338c905f6c0480a63bdacfa
selcuk <selcuk@DEVELOPER1>
2016-08-17 15:23:01 +0300

    command update

--- Kangal/SqlConnectionExtensions.cs
+++ Kangal/SqlConnectionExtensions.cs
@@ -24,24 +24,23 @@ namespace Kangal
 
         public static int Save<T>(this SqlConnection connection, IEnumerable<T> list, SqlTransaction transaction = null, string tableName = null)
         {
-            var enumerable = list as IList<T> ?? list.ToList();
-            if (enumerable == null || !enumerable.Any()) throw new ArgumentNullException("list");
-            tableName = string.IsNullOrEmpty(tableName) ? enumerable.FirstOrDefault().GetType().Name : tableName;
+            list = list.ToList();
+            if (list == null || !list.Any()) throw new ArgumentNullException(nameof(list));
+            tableName = string.IsNullOrEmpty(tableName) ? list.First().GetType().Name : tableName;
             var columns = string.Join(",",
-                enumerable.FirstOrDefault().GetType().GetProperties().Select(e => "@" + e.Name));
+                list.First().GetType().GetProperties().Select(e => "@" + e.Name));
             var query = $"INSERT INTO {tableName} ({columns.Replace("@", "")}) VALUES ({columns});";
             var affect = 0;
             var command = connection.CreateCommand();
             if (transaction != null) command.Transaction = transaction;
 
-            foreach (var item in enumerable)
+            foreach (var item in list)
             {
                 command.CommandText = query;
                 command.Parameters.AddRange(
-                    Helpers.SqlParameterHelper.CreateSqlParameters(item.GetType().GetProperties().ToArray(), item));
+                    Helpers.SqlParameterHelper.CreateSqlParameters(item.GetType().GetProperties(), item));
                 affect += command.ExecuteNonQuery();
                 command.Parameters.Clear();
-
             }
             return affect;
         }
