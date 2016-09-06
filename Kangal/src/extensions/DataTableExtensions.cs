00a4c6eb011d8dba449f855dfb2fbdc949c1e35e
selcuk <selcuk@DEVELOPER1>
2016-09-06 18:17:07 +0300

    test

--- Kangal/src/extensions/DataTableExtensions.cs
+++ Kangal/src/extensions/DataTableExtensions.cs
@@ -30,9 +30,8 @@ namespace Kangal
 
                     foreach (var property in generic.GetType().GetProperties())
                     {
-                        var ignoreAttribute =
-                            property.GetCustomAttributes(typeof(IgnoreAttribute), false).FirstOrDefault();
-                        if(ignoreAttribute != null) continue;
+                        var ignoreAttribute = (IgnoreAttribute)property.GetCustomAttributes(typeof(IgnoreAttribute), false).FirstOrDefault();
+                        if (ignoreAttribute != null) continue;
 
                         var columnAliasAttribute =
                             property.GetCustomAttributes(typeof(ColumnAliasAttribute), false)
